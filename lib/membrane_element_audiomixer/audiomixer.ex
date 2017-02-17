16855b87eb9d3e74a920f38a5ae6a1597c777f69
Mateusz Front <mateusz.front@swmansion.com>
2017-02-17 17:31:05 +0100

    WIP: implementing element logic

--- lib/membrane_element_audiomixer/audiomixer.ex
+++ lib/membrane_element_audiomixer/audiomixer.ex
@@ -1,24 +1,22 @@
 defmodule Membrane.Element.AudioMixer.MixerOptions do
-  defstruct \
-    pads_count: 5
 end
 
 defmodule Membrane.Element.AudioMixer.Mixer do
 
+  import Enum
   use Membrane.Element.Base.Filter
   alias Membrane.Caps.Audio.Raw
   alias Membrane.Element.AudioMixer.MixerOptions
 
   def_known_source_pads %{
-    :sink => {:always, [
-      %Raw{format: :s32le},
-      %Raw{format: :s16le},
-      %Raw{format: :u32le},
-      %Raw{format: :u16le},
-      %Raw{format: :s8},
-      %Raw{format: :u8},
-    ]}
+    :sink0 => {:always, []},
+    :sink1 => {:always, []},
+    :sink2 => {:always, []}
   }
+
+  @source_pads [:sink0, :sink1, :sink2]
+
+  def source_pads, do: @source_pads
 
   def_known_sink_pads %{
     :source => {:always, [
@@ -31,12 +29,23 @@ defmodule Membrane.Element.AudioMixer.Mixer do
     ]}
   }
 
-  @doc false
-  def handle_prepare(%MixerOptions{pads_count: pads_count}) do
+  defp empty_queue do
+    @source_pads |> flat_map( &%{&1 => << >>} )
   end
 
   @doc false
-  def handle_buffer({:sink, %Membrane.Buffer{payload: payload} = buffer}, %{} = state) do
+  def handle_prepare(_) do
+    {:ok, %{queue: empty_queue()}}
+  end
+
+  @doc false
+  def handle_caps({_sink, caps}, state) do
+    {:ok, %{state | queue: empty_queue(), caps: caps}}
+  end
+
+  @doc false
+  def handle_buffer({sink, %Membrane.Buffer{payload: payload} = buffer}, %{caps: caps, queue: queue} = state) do
+    
   end
 
   @doc false
