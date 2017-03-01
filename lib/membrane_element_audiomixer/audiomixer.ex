33d4ca9a2f0b1d56e31a2bad7bd4747459c3efb2
Mateusz Front <mateusz.front@swmansion.com>
2017-03-01 14:03:27 +0100

    added handling overflows and underflows (clipping)

--- lib/membrane_element_audiomixer/audiomixer.ex
+++ lib/membrane_element_audiomixer/audiomixer.ex
@@ -48,18 +48,25 @@ defmodule Membrane.Element.AudioMixer.Mixer do
 
   @doc false
   def handle_buffer({:sink, %Membrane.Buffer{payload: payload} = buffer}, %{sample_size: sample_size} = state) do
+    max_sample_value = (1 <<< (8*sample_size-1)) - 1
+    min_sample_value = -(1 <<< (8*sample_size-1))
     result = payload
       |> map(
         fn e -> e
           |> :binary.bin_to_list
           |> chunk(sample_size)
-          |> map(&to_string/1)
+          |> map(&:erlang.list_to_binary/1)
           |> map(&case &1 do <<s::little-signed-integer-size(sample_size)-unit(8)>> -> s end)
         end
       )
       |> zip
       |> map(&Tuple.to_list/1)
       |> map(&sum/1)
+      |> map(&cond do
+          &1 > max_sample_value -> max_sample_value
+          &1 < min_sample_value -> min_sample_value
+          true -> &1
+        end)
       |> map(&<<&1::little-signed-integer-size(sample_size)-unit(8)>>)
 
     {:ok, result}
--- spec/membrane_element_audiomixer/audiomixer_spec.exs
+++ spec/membrane_element_audiomixer/audiomixer_spec.exs
@@ -1,16 +1,31 @@
 defmodule Membrane.Element.AudioMixer.MixerSpec do
   use ESpec, async: false
+  use Bitwise
 
   describe ".handle_buffer/1" do
     let :state, do: %{sample_size: sample_size}
     let :sample_size, do: 2
     let :buffer, do: %Membrane.Buffer{payload: payload}
-    let :payload, do: [<<1, 0, 3, 0, 5>>, <<2, 0, 4, 0, 6>>]
-    it "should return ok result" do
-      expect(described_module.handle_buffer({:sink, buffer}, state)).to be_ok_result
+    context "in usual case" do
+      let :payload, do: [<<1, 0, 3, 0, 5>>, <<2, 0, 4, 0, 6>>]
+      it "should return ok result" do
+        expect(described_module.handle_buffer({:sink, buffer}, state)).to be_ok_result
+      end
+      it "should properly sum chunks" do
+        expect(described_module.handle_buffer({:sink, buffer}, state)).to eq {:ok, [<<3, 0>>, <<7, 0>>]}
+      end
     end
-    it "should properly sum chunks" do
-      expect(described_module.handle_buffer({:sink, buffer}, state)).to eq {:ok, [<<3, 0>>, <<7, 0>>]}
+    context "if there is overflow" do
+      let :payload, do: [<<255, 127>>, <<1, 0>>]
+      it "should cut value" do
+        expect(described_module.handle_buffer({:sink, buffer}, state)).to eq {:ok, [<<255, 127>>]}
+      end
+    end
+    context "if there is underflow" do
+      let :payload, do: [<<0,128>>, <<255,255>>]
+      it "should cut value" do
+        expect(described_module.handle_buffer({:sink, buffer}, state)).to eq {:ok, [<<0, 128>>]}
+      end
     end
   end
 end
