2444ccda4043071a4e62c4683255c482458ba4d3
Mateusz Front <mateusz.front@swmansion.com>
2017-03-01 12:46:12 +0100

    added mixing, supports only signed ints, does not deal with overflows

--- lib/membrane_element_audiomixer/audiomixer.ex
+++ lib/membrane_element_audiomixer/audiomixer.ex
@@ -4,14 +4,20 @@ end
 defmodule Membrane.Element.AudioMixer.Mixer do
 
   import Enum
+  use Bitwise
   use Membrane.Element.Base.Filter
   alias Membrane.Caps.Audio.Raw
   alias Membrane.Element.AudioMixer.MixerOptions
 
   def_known_source_pads %{
-    :sink0 => {:always, []},
-    :sink1 => {:always, []},
-    :sink2 => {:always, []}
+    :sink => {:always, [
+      %Raw{format: :s32le},
+      %Raw{format: :s16le},
+      %Raw{format: :u32le},
+      %Raw{format: :u16le},
+      %Raw{format: :s8},
+      %Raw{format: :u8},
+    ]}
   }
 
   @source_pads [:sink0, :sink1, :sink2]
@@ -29,23 +35,34 @@ defmodule Membrane.Element.AudioMixer.Mixer do
     ]}
   }
 
-  defp empty_queue do
-    @source_pads |> flat_map( &%{&1 => << >>} )
-  end
-
   @doc false
   def handle_prepare(_) do
-    {:ok, %{queue: empty_queue()}}
+    {:ok}
   end
 
   @doc false
-  def handle_caps({_sink, caps}, state) do
-    {:ok, %{state | queue: empty_queue(), caps: caps}}
+  def handle_caps({:sink, %Membrane.Caps.Audio.Raw{format: format} = caps}, state) do
+    {:ok, sample_size} = Raw.format_to_sample_size(format)
+    {:ok, %{state | sample_size: sample_size}}
   end
 
   @doc false
-  def handle_buffer({sink, %Membrane.Buffer{payload: payload} = buffer}, %{caps: caps, queue: queue} = state) do
-    
+  def handle_buffer({:sink, %Membrane.Buffer{payload: payload} = buffer}, %{sample_size: sample_size} = state) do
+    result = payload
+      |> map(
+        fn e -> e
+          |> :binary.bin_to_list
+          |> chunk(sample_size)
+          |> map(&to_string/1)
+          |> map(&case &1 do <<s::little-signed-integer-size(sample_size)-unit(8)>> -> s end)
+        end
+      )
+      |> zip
+      |> map(&Tuple.to_list/1)
+      |> map(&sum/1)
+      |> map(&<<&1::little-signed-integer-size(sample_size)-unit(8)>>)
+
+    {:ok, result}
   end
 
   @doc false
--- spec/membrane_element_audiomixer/audiomixer_spec.exs
+++ spec/membrane_element_audiomixer/audiomixer_spec.exs
@@ -1,4 +1,16 @@
 defmodule Membrane.Element.AudioMixer.MixerSpec do
   use ESpec, async: false
 
+  describe ".handle_buffer/1" do
+    let :state, do: %{sample_size: sample_size}
+    let :sample_size, do: 2
+    let :buffer, do: %Membrane.Buffer{payload: payload}
+    let :payload, do: [<<1, 0, 3, 0, 5>>, <<2, 0, 4, 0, 6>>]
+    it "should return ok result" do
+      expect(described_module.handle_buffer({:sink, buffer}, state)).to be_ok_result
+    end
+    it "should properly sum chunks" do
+      expect(described_module.handle_buffer({:sink, buffer}, state)).to eq {:ok, [<<3, 0>>, <<7, 0>>]}
+    end
+  end
 end
