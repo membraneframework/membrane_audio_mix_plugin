f096fd0e8804183cc9a4473ddb24c7acada976e1
Mateusz Front <mateusz.front@swmansion.com>
2017-03-29 13:59:17 +0200

    fixes and test update for dynamic sinks support in aligner

--- lib/membrane_element_audiomixer/audioaligner.ex
+++ lib/membrane_element_audiomixer/audioaligner.ex
@@ -19,24 +19,23 @@ defmodule Membrane.Element.AudioMixer.Aligner do
   use Membrane.Element.Base.Filter
   alias Membrane.Caps.Audio.Raw, as: Caps
   alias Membrane.Element.AudioMixer.AlignerOptions
-  alias Array
   alias Membrane.Time
 
-  @sink_types [
-      %Caps{format: :f32le},
-      %Caps{format: :s32le},
-      %Caps{format: :s16le},
-      %Caps{format: :u32le},
-      %Caps{format: :u16le},
-      %Caps{format: :s8},
-      %Caps{format: :u8},
-    ]
-
-  def_known_sink_pads %{
-    :sink0 => {:always, @sink_types},
-    :sink1 => {:always, @sink_types},
-    :sink2 => {:always, @sink_types},
-  }
+  # @sink_types [
+  #     %Caps{format: :f32le},
+  #     %Caps{format: :s32le},
+  #     %Caps{format: :s16le},
+  #     %Caps{format: :u32le},
+  #     %Caps{format: :u16le},
+  #     %Caps{format: :s8},
+  #     %Caps{format: :u8},
+  #   ]
+  #
+  # def_known_sink_pads %{
+  #   :sink0 => {:always, @sink_types},
+  #   :sink1 => {:always, @sink_types},
+  #   :sink2 => {:always, @sink_types},
+  # }
 
   def_known_source_pads %{
     :source => {:always, [
@@ -50,45 +49,42 @@ defmodule Membrane.Element.AudioMixer.Aligner do
     ]}
   }
 
-  @empty_queue @sink_pads |> into(Array.new, fn _ -> <<>> end)
-  @empty_to_drop @sink_pads |> into(Array.new, fn _ -> 0 end)
-
 
   @doc false
-  def handle_init(%AlignerOptions{chunk_time: chunk_time}) do
-    {:ok, sink_pads: %{}, chunk_time: chunk_time, timer: Nil, previous_tick: Nil}
+  def handle_init %AlignerOptions{chunk_time: chunk_time} do
+    #TODO: here only init sample size and sample rate to Nil, set them up in handle_caps / handle_new_sink
+    {:ok, %{sink_data: %{}, chunk_time: chunk_time, timer: Nil, previous_tick: Nil, sample_size: 2, sample_rate: 44100}}
   end
 
   @doc false
-  def handle_play(%{chunk_time: chunk_time} = state) do
+  def handle_play %{chunk_time: chunk_time} = state do
     {:ok, timer} = :timer.send_interval(chunk_time/(1 |> Time.millisecond) |> trunc, :tick)
     %{state | timer: timer, previous_tick: Time.native_monotonic_time}
   end
 
-  @doc false
-  def handle_caps(_sink, %Caps{sample_rate: sample_rate, format: format} = caps, state) do
-    sample_size = Caps.format_to_sample_size format
-    {
-      :ok,
-      [{:caps, {:source, caps}}],
-      %{state |
-        sample_size: sample_size,
-        sample_rate: sample_rate,
-        queue: @empty_queue,
-        to_drop: @empty_to_drop,
-      }
-    }
-  end
+  # @doc false
+  # def handle_caps(_sink, %Caps{sample_rate: sample_rate, format: format} = caps, state) do
+  #   sample_size = Caps.format_to_sample_size format
+  #   {
+  #     :ok,
+  #     [{:caps, {:source, caps}}],
+  #     %{state |
+  #       sample_size: sample_size,
+  #       sample_rate: sample_rate,
+  #     }
+  #   }
+  # end
 
   @doc false
-  def handle_other({:new_sink, sink, _caps}, %{sink_pads: sink_pads} = state) do
+  def handle_other {:new_sink, sink, _caps}, %{sink_data: sink_data} = state do
+    # TODO: handle and verify caps
     {:ok, [], %{state |
-      sink_pads: sink_pads |> Map.put(sink, %{queue: <<>>, to_drop: 0, first_play: true})
+      sink_data: sink_data |> Map.put(sink, %{queue: <<>>, to_drop: 0, first_play: true})
     }}
   end
   #TODO: add sink removal handler
 
-  defp update_sink_payload %{queue: queue, to_drop: to_drop} = sink_data do
+  defp update_sink_data payload, %{queue: queue, to_drop: to_drop} = sink_data do
     cut_payload = case payload do
       <<_::binary-size(to_drop)-unit(8)>> <> r -> r
       _ -> <<>>
@@ -100,19 +96,21 @@ defmodule Membrane.Element.AudioMixer.Aligner do
   end
 
   @doc false
-  def handle_other({sink, %Membrane.Buffer{payload: payload}}, %{sink_pads: sink_pads} = state) do
+  def handle_other {sink, %Membrane.Buffer{payload: payload}}, %{sink_data: sink_data} = state do
     {:ok, [], %{state |
-      sink_pads: sink_pads |> Map.update!(sink, update_sink_payload)
+      sink_data: sink_data |> Map.update!(sink, &(update_sink_data payload, &1))
     }}
   end
 
-  defp current_chunk_size(current_tick, previous_tick, sample_size, sample_rate) do
+  defp current_chunk_size current_tick, previous_tick, sample_size, sample_rate do
     duration = current_tick - previous_tick
     trunc sample_size*duration*sample_rate/Time.native_resolution
   end
 
-  defp extract_sink_data({sink, %{first_play: true} = sink_data}, chunk_size), do: {<<>>, %{sink_data | first_play: false}}
-  defp extract_sink_data({sink, %{queue: queue, to_drop: to_drop} = sink_data}, chunk_size) do
+  defp extract_sink_data {sink, %{first_play: true} = sink_data}, chunk_size do
+     {<<>>, %{sink_data | first_play: false}}
+  end
+  defp extract_sink_data {sink, %{queue: queue, to_drop: to_drop} = sink_data}, chunk_size do
     case queue do
       <<p::binary-size(chunk_size)-unit(8)>> <> r -> {p, {sink, %{sink_data | queue: r, to_drop: to_drop}}}
       _ -> {queue, {sink, %{sink_data | queue: <<>>, to_drop: chunk_size - byte_size(queue)}}}
@@ -121,21 +119,21 @@ defmodule Membrane.Element.AudioMixer.Aligner do
 
 
   @doc false
-  def handle_other(:tick, %{sink_pads: sink_pads, sample_size: sample_size, sample_rate: sample_rate, previous_tick: previous_tick} = state) do
+  def handle_other :tick, %{sink_data: sink_data, sample_size: sample_size, sample_rate: sample_rate, previous_tick: previous_tick} = state do
     current_tick = Time.native_monotonic_time
     chunk_size = current_chunk_size current_tick, previous_tick, sample_size, sample_rate
-    {data, sink_pads} = sink_pads
+    {data, sink_data} = sink_data
       |> map(&extract_sink_data &1, chunk_size)
       |> unzip
       |> case do {d, s} -> {d, s |> into(%{})} end
 
     remaining_samples_cnt = (chunk_size - byte_size(data |> max_by(&byte_size/1))) / sample_size |> Float.ceil |> trunc
 
-    {:ok, [{:send, {:source, %Membrane.Buffer{payload: %{data: data, remaining_samples_cnt: remaining_samples_cnt}}}}], %{state | sink_pads: sink_pads, previous_tick: current_tick}}
+    {:ok, [{:send, {:source, %Membrane.Buffer{payload: %{data: data, remaining_samples_cnt: remaining_samples_cnt}}}}], %{state | sink_data: sink_data, previous_tick: current_tick}}
   end
 
   @doc false
-  def handle_stop(%{timer: timer} = state) do
+  def handle_stop %{timer: timer} = state do
     {:ok, :cancel} = :timer.cancel timer
     {:ok, %{state | timer: Nil, previous_tick: Nil}}
   end
--- spec/membrane_element_audiomixer/audioaligner_spec.exs
+++ spec/membrane_element_audiomixer/audioaligner_spec.exs
@@ -10,92 +10,107 @@ defmodule Membrane.Element.AudioMixer.AlignerSpec do
   before do: allow(Membrane.Time).to accept(:native_resolution, fn -> 1 end)
 
   let :empty_queue, do: Array.from_list [<<>>,<<>>,<<>>]
-  let :simple_queue, do: Array.from_list [<<1,2,3,4,5,6>>,<<7,8,9,10,11,12>>,<<13,14,15,16,17,18>>]
+  let :simple_queue, do: Array.from_list
   let :empty_to_drop, do: Array.from_list [0, 0, 0]
+  defp to_sink_data list do
+    list
+      |> with_index
+      |> into(%{}, fn {{q, d}, i} -> {i, %{queue: q, to_drop: d, first_play: false}} end)
+  end
+  let :empty_sink_data, do: 0..2 |> map(fn _ -> {<<>>, 0} end) |> to_sink_data
+  let :simple_sink_data, do: [{<<1,2,3,4,5,6>>, 0},{<<7,8,9,10,11,12>>, 0},{<<13,14,15,16,17,18>>, 0}] |> to_sink_data
   let :caps, do: Nil
 
   describe ".handle_buffer/1" do
-    let :buffer, do: %Membrane.Buffer{payload: payload}
-    let :state, do: %{queue: queue, to_drop: to_drop}
-    context "in usual case" do
-      let :to_drop, do: empty_to_drop
-      let :queue, do: simple_queue
-      let :payload, do: <<9,8,7>>
-      it "should add buffer to queue" do
-        expect(described_module.handle_buffer(:sink0, caps, buffer, state)).to eq {:ok, [], %{state | queue: simple_queue |> Array.set(0, <<1,2,3,4,5,6,9,8,7>>)}}
-      end
-    end
-    context "if buffer needs to be cut" do
-      let :to_drop, do: empty_to_drop |> Array.set(1, 1)
-      let :queue, do: empty_queue
-      let :payload, do: <<9,8,7>>
-      it "should cut buffer and add it to queue" do
-        expect(described_module.handle_buffer(:sink1, caps, buffer, state)).to eq {:ok, [], %{
-            state | queue: queue |> Array.set(1, <<8,7>>), to_drop: empty_to_drop
-          }}
-      end
-    end
+
   end
 
   describe ".handle_other/1" do
-    let :state, do: %{queue: queue, sample_rate: 1000, sample_size: 3, previous_tick: 0.098, to_drop: to_drop}
-    let :to_drop, do: empty_to_drop
-    defp handle_other_ok_result([data: data, remaining_samples_cnt: remaining_samples_cnt, state: state]) do
-      {
-        :ok,
-        [{:send, {:source, %Membrane.Buffer{payload: %{data: data, remaining_samples_cnt: remaining_samples_cnt}}}}],
-        %{state | previous_tick: now}
-      }
+    context "{sink, Membrane.Buffer}" do
+      let :buffer, do: %Membrane.Buffer{payload: payload}
+      let :state, do: %{sink_data: sink_data}
+      context "in usual case" do
+        let :sink_data, do: simple_sink_data
+        let :payload, do: <<9,8,7>>
+        it "should add buffer to queue" do
+          expect(described_module.handle_other({0, buffer}, state)).to eq {
+            :ok, [], %{state | sink_data: simple_sink_data |> Map.update!(0, &%{&1 | queue: <<1,2,3,4,5,6,9,8,7>>})}
+          }
+        end
+      end
+      context "if buffer needs to be cut" do
+        let :sink_data, do: empty_sink_data |> Map.update!(1, &%{&1 | to_drop: 1})
+        let :payload, do: <<9,8,7>>
+        it "should cut buffer and add it to queue" do
+          expect(described_module.handle_other({1, buffer}, state)).to eq {:ok, [], %{
+              state | sink_data: empty_sink_data |> Map.update!(1, &%{&1 | queue: <<8,7>>})
+            }}
+        end
+      end
     end
+    context ":tick" do
+      let :state, do: %{sink_data: sink_data, sample_rate: 1000, sample_size: 3, previous_tick: 0.098}
+      defp handle_other_ok_result([data: data, remaining_samples_cnt: remaining_samples_cnt, state: state]) do
+        {
+          :ok,
+          [{:send, {:source, %Membrane.Buffer{payload: %{data: data, remaining_samples_cnt: remaining_samples_cnt}}}}],
+          %{state | previous_tick: now}
+        }
+      end
 
-    context "in usual case" do
-      let :queue, do: simple_queue
-      it "should parse and send queue as a buffer" do
-        expect(described_module.handle_other :tick, state).to eq handle_other_ok_result([
-          data: queue |> Array.to_list,
-          remaining_samples_cnt: 0,
-          state: %{state | queue: empty_queue}]
-        )
+      defp queues sink_data do
+        sink_data |> Map.values |> map(fn %{queue: q} -> q end)
       end
-    end
-    context "if sizes of queues differ" do
-      context "and one of them lacks data" do
-        let :queue, do: simple_queue |> Array.set(1, <<1,2,3>>)
-        it "should forward queue and update to_drop" do
+
+      context "in usual case" do
+        let :sink_data, do: simple_sink_data
+        it "should parse and send queue as a buffer" do
           expect(described_module.handle_other :tick, state).to eq handle_other_ok_result([
-            data: queue |> Array.to_list,
+            data: sink_data |> queues,
             remaining_samples_cnt: 0,
-            state: %{state | queue: empty_queue, to_drop: Array.from_list [0, 3, 0]}
-          ])
+            state: %{state | sink_data: empty_sink_data}]
+          )
         end
       end
-      context "and one of them excesses data" do
-        let :queue, do: simple_queue |> Array.set(1, <<1,2,3,4,5,6,7,8,9>>)
-        it "should forward queue and store excess in the new queue" do
-          expect(described_module.handle_other :tick, state).to eq handle_other_ok_result([
-            data: queue |> Array.set(1, <<1,2,3,4,5,6>>) |> Array.to_list,
-            remaining_samples_cnt: 0,
-            state: %{state | queue: empty_queue |> Array.set(1, <<7,8,9>>)}
-          ])
-        end
-      end
-      context "and all of them lack data" do
-        let :queue, do: Array.from_list [<<1,2,3>>, <<1,2,3>>, <<>>]
-        it "should forward queue, update to_drop and set remaining_samples_cnt" do
-          expect(described_module.handle_other :tick, state).to eq handle_other_ok_result([
-            data: queue |> Array.to_list,
-            remaining_samples_cnt: 1,
-            state: %{state | queue: empty_queue, to_drop: Array.from_list [3, 3, 6]}
-          ])
-        end
-        context "and size of longest path is not an integer multiplication of sample size" do
-          let :queue, do: Array.from_list [<<>>, <<1,2,3,4,5>>, <<>>]
-          it "should forward queue, update to_drop and set remaining_samples_cnt skipping incomplete sample" do
+      context "if sizes of queues differ" do
+        context "and one of them lacks data" do
+          let :sink_data, do: simple_sink_data |> Map.update!(1, &%{&1 | queue: <<1,2,3>>})
+          it "should forward queue and update to_drop" do
             expect(described_module.handle_other :tick, state).to eq handle_other_ok_result([
-              data: queue |> Array.to_list,
-              remaining_samples_cnt: 1,
-              state: %{state | queue: empty_queue, to_drop: Array.from_list [6, 1, 6]}
+              data: sink_data |> queues,
+              remaining_samples_cnt: 0,
+              state: %{state | sink_data: empty_sink_data |> Map.update!(1, &%{&1 | to_drop: 3})}
             ])
+          end
+        end
+        context "and one of them excesses data" do
+          let :sink_data, do: simple_sink_data |> Map.update!(1, &%{&1 | queue: <<1,2,3,4,5,6,7,8,9>>})
+          it "should forward queue and store excess in the new queue" do
+            expect(described_module.handle_other :tick, state).to eq handle_other_ok_result([
+              data: simple_sink_data |> Map.update!(1, &%{&1 | queue: <<1,2,3,4,5,6>>}) |> queues,
+              remaining_samples_cnt: 0,
+              state: %{state | sink_data: empty_sink_data |> Map.update!(1, &%{&1 | queue: <<7,8,9>>})}
+            ])
+          end
+        end
+        context "and all of them lack data" do
+          let :sink_data, do: [{<<1,2,3>>, 0}, {<<1,2,3>>, 0}, {<<>>, 0}] |> to_sink_data
+          it "should forward queue, update to_drop and set remaining_samples_cnt" do
+            expect(described_module.handle_other :tick, state).to eq handle_other_ok_result([
+              data: sink_data |> queues,
+              remaining_samples_cnt: 1,
+              state: %{state | sink_data: [{<<>>, 3}, {<<>>, 3}, {<<>>, 6}] |> to_sink_data}
+            ])
+          end
+          context "and size of longest path is not an integer multiplication of sample size" do
+            let :sink_data, do: empty_sink_data |> Map.update!(1, &%{&1 | queue: <<1,2,3,4,5>>})
+            it "should forward queue, update to_drop and set remaining_samples_cnt skipping incomplete sample" do
+              expect(described_module.handle_other :tick, state).to eq handle_other_ok_result([
+                data: sink_data |> queues,
+                remaining_samples_cnt: 1,
+                state: %{state | sink_data: [{<<>>, 6}, {<<>>, 1}, {<<>>, 6}] |> to_sink_data}
+              ])
+            end
           end
         end
       end
