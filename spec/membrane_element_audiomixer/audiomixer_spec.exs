f645ebc0634540bcb8a8c08b1691fb959007d83c
Mateusz Front <mateusz.front@swmansion.com>
2017-05-17 17:53:33 +0200

    adjusted mixer tests

--- spec/membrane_element_audiomixer/audiomixer_spec.exs
+++ spec/membrane_element_audiomixer/audiomixer_spec.exs
@@ -26,14 +26,14 @@ defmodule Membrane.Element.AudioMixer.MixerSpec do
 
     context "in usual case" do
       let :format, do: :s16le
-      let :payload, do: [<<1, 0, 3, 0>>, <<2, 0, 4, 0>>]
+      let :payload, do: [[<<1, 0, 3, 0>>], [<<2>>, <<0, 4, 0>>]]
       it "should properly sum chunks" do
         expect(described_module.handle_buffer(:sink, caps, buffer, state)).to eq handle_buffer_ok_result [payload: <<3, 0, 7, 0>>, state: state]
       end
     end
     context "if lenghts of chunks differ" do
       let :format, do: :s16le
-      let :payload, do: [<<1, 0, 3, 0>>, <<2, 0, 4, 0, 5, 0>>]
+      let :payload, do: [[<<1, 0>>, <<3, 0>>], [<<2, 0, 4, 0, 5, 0>>]]
       it "should sum adjacent samples and copy the rest" do
         expect(described_module.handle_buffer(:sink, caps, buffer, state)).to eq handle_buffer_ok_result [payload: <<3, 0, 7, 0, 5, 0>>, state: state]
       end
@@ -41,13 +41,13 @@ defmodule Membrane.Element.AudioMixer.MixerSpec do
     context "if format is signed" do
       let :format, do: :s16le
       context "and there is overflow" do
-        let :payload, do: [<<255, 127>>, <<1, 0>>]
+        let :payload, do: [[<<255, 127>>], [<<1, 0>>]]
         it "should cut value" do
           expect(described_module.handle_buffer(:sink, caps, buffer, state)).to eq handle_buffer_ok_result [payload: <<255, 127>>, state: state]
         end
       end
       context "and there is underflow" do
-        let :payload, do: [<<0,128>>, <<255,255>>]
+        let :payload, do: [[<<0,128>>], [<<255,255>>]]
         it "should cut value" do
           expect(described_module.handle_buffer(:sink, caps, buffer, state)).to eq handle_buffer_ok_result [payload: <<0, 128>>, state: state]
         end
@@ -56,7 +56,7 @@ defmodule Membrane.Element.AudioMixer.MixerSpec do
     context "if format is unsigned" do
       let :format, do: :u16le
       let :discontinuity, do: 0
-      let :payload, do: [<<255, 255>>, <<1, 0>>]
+      let :payload, do: [[<<255>>, <<255>>], [<<1, 0>>]]
       context "and there is overflow" do
         it "should cut value" do
           expect(described_module.handle_buffer(:sink, caps, buffer, state)).to eq handle_buffer_ok_result [payload: <<255,255>>, state: state]
@@ -73,13 +73,13 @@ defmodule Membrane.Element.AudioMixer.MixerSpec do
     context "if some paths contain incomplete sample" do
       let :format, do: :s16le
       context "and one of them is the longest path" do
-        let :payload, do: [<<1, 0, 3, 0, 5>>, <<2, 0, 4, 0>>, <<1,0,3>>]
+        let :payload, do: [[<<1, 0, 3, 0, 5>>], [<<2, 0, 4, 0>>], [<<1,0,3>>]]
         it "should skip incomplete samples" do
           expect(described_module.handle_buffer(:sink, caps, buffer, state)).to eq handle_buffer_ok_result [payload: <<4, 0, 7, 0>>, state: state]
         end
       end
       context "and none of them is the longest path" do
-        let :payload, do: [<<1, 0, 3, 0>>, <<2>>, <<1, 0, 3>>]
+        let :payload, do: [[<<1, 0, 3, 0>>], [<<2>>], [<<1, 0, 3>>]]
         it "should skip incomplete samples" do
           expect(described_module.handle_buffer(:sink, caps, buffer, state)).to eq handle_buffer_ok_result [payload: <<2, 0, 3, 0>>, state: state]
         end
