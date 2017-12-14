7aea3c06d30e4d241e21ca45e217beb74116ae25
Mateusz Front <mateusz.front@swmansion.com>
2017-12-14 12:26:29 +0100

    refactor, fixed a few RC bugs related to supplying demands on channel removal

--- lib/membrane_element_audiomixer/mixer.ex
+++ lib/membrane_element_audiomixer/mixer.ex
@@ -4,7 +4,7 @@ defmodule Membrane.Element.AudioMixer.Mixer do
   alias Membrane.Element.AudioMixer.DoMix
   alias Membrane.{Buffer, Event}
   alias Membrane.Caps.Audio.Raw, as: Caps
-  use Membrane.Mixins.Log
+  use Membrane.Mixins.Log, tags: :membrane_element_audiomixer
 
   def_known_source_pads %{
     :source => {:always, :pull, :any}
@@ -16,16 +16,15 @@ defmodule Membrane.Element.AudioMixer.Mixer do
   def handle_init(_) do
     state = %{
       sinks: %{},
-      ending_sinks: [],
     }
     {:ok, state}
   end
 
-  def handle_pad_added(pad, :sink, state) do
-    {:ok, state |> Helper.Map.put_in([:sinks, pad], %{queue: <<>>})}
+  def handle_pad_added(_pad, :sink, state) do
+    {:ok, state}
   end
 
-  def handle_pad_removed(pad, state) do
+  def handle_pad_removed({:dynamic, :sink, _} = pad, state) do
     {:ok, state |> Helper.Map.remove_in([:sinks, pad])}
   end
 
@@ -38,54 +37,86 @@ defmodule Membrane.Element.AudioMixer.Mixer do
     {{:ok, demands}, state}
   end
 
-  def handle_event({:dynamic, :sink, _id} = sink, %Event{type: :eos}, _, state) do
-    {:ok, state |> Map.update!(:ending_sinks, & [sink | &1])}
+  def handle_event({:dynamic, :sink, _id} = sink, %Event{type: :sos}, _, state) do
+    info "mixer sos #{inspect sink}"
+    new_channel = %Event{type: :channel_added, stick_to: :buffer}
+    state = state |> Helper.Map.put_in([:sinks, sink], %{queue: <<>>, eos: false})
+    {{:ok, event: {:source, new_channel}}, state}
+  end
+  def handle_event({:dynamic, :sink, _id} = sink, %Event{type: :eos}, params, state) do
+    %{caps: caps} = params
+    info "mixer eos #{inspect sink}"
+    state = state |> Helper.Map.update_in([:sinks, sink], & %{&1 | eos: true})
+    with {{:ok, actions}, state} <- do_handle_process(caps, state) do
+      {{:ok, [event: {:source, %Event{type: :channel_removed}}] ++ actions}, state}
+    end
   end
   def handle_event(pad, event, params, state) do
     super(pad, event, params, state)
   end
 
-  def handle_process1(
-    sink, %Buffer{payload: payload}, %{caps: caps}, %{sinks: sinks, ending_sinks: ending} = state
-  ) do
-    sinks = sinks |> Helper.Map.update_in([sink, :queue], & &1 <> payload)
+  def handle_process1(sink, buffer, params, state) do
 
+    %Buffer{payload: payload} = buffer
+    %{sinks: sinks} = state
+    %{caps: caps} = params
     time_frame = Caps.format_to_sample_size!(caps.format) * caps.channels
 
-    min_size = min_size(sinks, time_frame)
+    {size, sinks} = sinks
+      |> Helper.Map.get_and_update_in([sink, :queue],
+        & {&1 |> byte_size, &1 <> payload})
 
-    if min_size == 0 do
-      {:ok, %{state | sinks: sinks}}
+    if size >= time_frame do
+      do_handle_process caps, %{state | sinks: sinks}
     else
-      {payload, sinks} = mix(sinks, min_size, caps)
-
-      {ending, sinks} = remove_finished_sinks(ending, sinks, time_frame)
-
-      {
-        {:ok, buffer: {:source, %Buffer{payload: payload}}},
-        %{state | sinks: sinks, ending_sinks: ending}
-      }
+      {:ok, %{state | sinks: sinks}}
     end
   end
 
+  defp do_handle_process(caps, state) do
+    %{sinks: sinks} = state
+    time_frame = Caps.format_to_sample_size!(caps.format) * caps.channels
+    mix_size = mix_size(sinks, time_frame)
+    if mix_size >= time_frame do
+      {payload, sinks} = mix(sinks, mix_size, caps)
 
-  defp min_size(sinks, time_frame) do
+      sinks = sinks |> remove_finished_sinks(time_frame)
+
+      {{:ok, buffer: {:source, %Buffer{payload: payload}}}, %{state | sinks: sinks}}
+    else
+      {{:ok, []}, %{state | sinks: sinks}}
+    end
+  end
+
+  defp mix_size(sinks, time_frame) do
+    fallback = fn ->
+        sinks
+          |> Enum.map(fn {_sink, %{queue: q}} -> byte_size q end)
+          |> Enum.max
+      end
     sinks
-      |> Enum.map(fn {_sink, %{queue: q}} -> q end)
-      |> Enum.map(&byte_size/1)
-      |> Enum.min
+      |> Enum.flat_map(fn
+          {_sink, %{queue: q, eos: false}} -> [byte_size q]
+          _ -> []
+        end)
+      |> Enum.min(fallback)
       |> int_part(time_frame)
   end
 
-  defp mix(sinks, min_size, _caps) when map_size(sinks) == 1 do
+  defp mix(sinks, mix_size, _caps) when map_size(sinks) == 1 do
     {sink, data} = sinks |> Map.to_list |> hd
-    %{queue: <<p::binary-size(min_size), q::binary>>} = data
+    %{queue: <<p::binary-size(mix_size), q::binary>>} = data
     {p, %{sink => %{data | queue: q}}}
   end
 
-  defp mix(sinks, min_size, caps) do
+  defp mix(sinks, mix_size, caps) do
     {payloads, sinks} = sinks
-      |> Enum.map(fn {s, %{queue: <<p::binary-size(min_size), nq::binary>>}} -> {p, {s, %{queue: nq}}} end)
+      |> Enum.map(fn
+          {sink, %{queue: <<payload::binary-size(mix_size), nq::binary>>} = data} ->
+            {payload, {sink, %{data | queue: nq}}}
+          {sink, %{queue: payload} = data} ->
+            {payload, {sink, %{data | queue: <<>>}}}
+        end)
       |> Enum.unzip
 
     payload = payloads |> DoMix.mix(caps)
@@ -93,15 +124,13 @@ defmodule Membrane.Element.AudioMixer.Mixer do
     {payload, sinks}
   end
 
-  defp remove_finished_sinks(ending_sinks, sinks, time_frame) do
-    {ending, sinks} = ending_sinks |> Enum.flat_map_reduce(sinks, fn sink, sinks ->
-        case sinks[sink] do
-          %{queue: q} when byte_size(q) < time_frame -> {[], sinks |> Map.delete(sink)}
-          nil -> {[], sinks}
-          _ -> {[sink], sinks}
-        end
-      end)
-    {ending, sinks}
+  defp remove_finished_sinks(sinks, time_frame) do
+    sinks
+      |> Enum.flat_map(fn
+          {_sink, %{queue: q, eos: true}} when byte_size(q) < time_frame -> []
+          sink_data -> [sink_data]
+        end)
+      |> Map.new
   end
 
 end
