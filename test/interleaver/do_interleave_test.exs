f45b7a4f6d8ae950f31795ec4657fb2f8baccf32
Agnieszka Dutka <agud100@gmail.com>
2021-08-17 11:05:34 +0200

    1adding tests

--- test/interleaver/do_interleave_test.exs
+++ test/interleaver/do_interleave_test.exs
@@ -10,12 +10,9 @@ defmodule Membrane.DdInterleaveTest do
 
   use ExUnit.Case, async: true
 
-  import Membrane.AudioInterleaver
   import Membrane.AudioMixer.DoInterleave
 
-  alias Membrane.AudioInterleaver
   alias Membrane.AudioMixer.DoInterleave
-  alias Membrane.Caps.Audio.Raw, as: Caps
 
   require Membrane.Logger
 
@@ -23,32 +20,56 @@ defmodule Membrane.DdInterleaveTest do
     {{Membrane.Pad, :input, key}, %{end_of_stream: false, queue: queue}}
   end
 
-  test "test interleave for different sample_size" do
-    pads =
-      Map.new([
-        to_pad(1, <<1, 2, 3, 4, 5, 6, 7, 8>>),
-        to_pad(2, <<90, 100, 110, 120, 130, 140, 150, 160>>)
-      ])
+  describe "DoInterleaver interleave test" do
+    test "test interleave for different sample_size" do
+      pads =
+        Map.new([
+          to_pad(1, <<1, 2, 3, 4, 5, 6, 7, 8>>),
+          to_pad(2, <<90, 100, 110, 120, 130, 140, 150, 160>>)
+        ])
 
-    order = [1, 2]
-    bytes_per_channel = 4
+      order = [1, 2]
+      bytes_per_channel = 4
 
-    interleaved1 = <<1, 90, 2, 100, 3, 110, 4, 120>>
-    interleaved2 = <<1, 2, 90, 100, 3, 4, 110, 120>>
-    interleaved3 = <<1, 2, 3, 90, 100, 110, 4, 120>>
-    interleaved4 = <<1, 2, 3, 4, 90, 100, 110, 120>>
+      interleaved1 = <<1, 90, 2, 100, 3, 110, 4, 120>>
+      interleaved2 = <<1, 2, 90, 100, 3, 4, 110, 120>>
+      interleaved3 = <<1, 2, 3, 90, 100, 110, 4, 120>>
+      interleaved4 = <<1, 2, 3, 4, 90, 100, 110, 120>>
 
-    new_pads = Map.new([to_pad(1, <<5, 6, 7, 8>>), to_pad(2, <<130, 140, 150, 160>>)])
+      new_pads = Map.new([to_pad(1, <<5, 6, 7, 8>>), to_pad(2, <<130, 140, 150, 160>>)])
 
-    expected1 = {interleaved1, new_pads}
-    expected2 = {interleaved2, new_pads}
-    expected3 = {interleaved3, new_pads}
-    expected4 = {interleaved4, new_pads}
+      expected1 = {interleaved1, new_pads}
+      expected2 = {interleaved2, new_pads}
+      expected3 = {interleaved3, new_pads}
+      expected4 = {interleaved4, new_pads}
 
-    assert expected1 == DoInterleave.interleave(bytes_per_channel, 1, pads, order)
-    assert expected2 == DoInterleave.interleave(bytes_per_channel, 2, pads, order)
-    assert expected3 == DoInterleave.interleave(bytes_per_channel, 3, pads, order)
-    assert expected4 == DoInterleave.interleave(bytes_per_channel, 4, pads, order)
+      assert expected1 == DoInterleave.interleave(bytes_per_channel, 1, pads, order)
+      assert expected2 == DoInterleave.interleave(bytes_per_channel, 2, pads, order)
+      assert expected3 == DoInterleave.interleave(bytes_per_channel, 3, pads, order)
+      assert expected4 == DoInterleave.interleave(bytes_per_channel, 4, pads, order)
+    end
+
+    test "test interleave order" do
+      pads =
+        Map.new([
+          to_pad(1, <<1, 2, 3, 4, 5, 6, 7, 8>>),
+          to_pad(2, <<90, 100, 110, 120, 130, 140, 150, 160>>)
+        ])
+
+      order = [2, 1]
+      bytes_per_channel = 4
+
+      interleaved1 = <<90, 1, 100, 2, 110, 3, 120, 4>>
+      interleaved2 = <<90, 100, 1, 2, 110, 120, 3, 4>>
+
+      new_pads = Map.new([to_pad(1, <<5, 6, 7, 8>>), to_pad(2, <<130, 140, 150, 160>>)])
+
+      expected1 = {interleaved1, new_pads}
+      expected2 = {interleaved2, new_pads}
+
+      assert expected1 == DoInterleave.interleave(bytes_per_channel, 1, pads, order)
+      assert expected2 == DoInterleave.interleave(bytes_per_channel, 2, pads, order)
+    end
   end
 
   test "splits binaries in reversed chunks" do
