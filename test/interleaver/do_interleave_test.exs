3cc6383505c6618ba8bb1ec1e98dc63c23803e40
Agnieszka Dutka <agud100@gmail.com>
2021-08-18 15:14:19 +0200

    tests fixes

--- test/interleaver/do_interleave_test.exs
+++ test/interleaver/do_interleave_test.exs
@@ -35,7 +35,7 @@ defmodule Membrane.DdInterleaveTest do
 
       Enum.each(cases, fn {sample_size, expected_binary} ->
         expected = {expected_binary, new_pads}
-        assert expected == DoInterleave.interleave(bytes_per_channel, sample_size, pads, order)
+        assert DoInterleave.interleave(bytes_per_channel, sample_size, pads, order) == expected
       end)
     end
 
@@ -48,17 +48,17 @@ defmodule Membrane.DdInterleaveTest do
 
       order = [2, 1]
       bytes_per_channel = 4
-
-      interleaved1 = <<90, 1, 100, 2, 110, 3, 120, 4>>
-      interleaved2 = <<90, 100, 1, 2, 110, 120, 3, 4>>
-
       new_pads = Map.new([to_pad(1, <<5, 6, 7, 8>>), to_pad(2, <<130, 140, 150, 160>>)])
 
-      expected1 = {interleaved1, new_pads}
-      expected2 = {interleaved2, new_pads}
+      cases = [
+        {1, <<90, 1, 100, 2, 110, 3, 120, 4>>},
+        {2, <<90, 100, 1, 2, 110, 120, 3, 4>>}
+      ]
 
-      assert expected1 == DoInterleave.interleave(bytes_per_channel, 1, pads, order)
-      assert expected2 == DoInterleave.interleave(bytes_per_channel, 2, pads, order)
+      Enum.each(cases, fn {sample_size, expected_binary} ->
+        expected = {expected_binary, new_pads}
+        assert DoInterleave.interleave(bytes_per_channel, sample_size, pads, order) == expected
+      end)
     end
   end
 end
