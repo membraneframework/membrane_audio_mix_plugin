de6dce173fdba6904e7b22c5185f52a60fda65c4
agga1 <agud100@gmail.com>
2021-08-31 17:49:42 +0200

    bump version 0.1.0 -> 0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
-defmodule Membrane.AudioMixer.Mixfile do
+defmodule Membrane.AudioMix.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @github_url "https://github.com/membraneframework/membrane_audio_mix_plugin"
 
   def project do
