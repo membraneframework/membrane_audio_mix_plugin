826979127919383ed61968bdedf19d2a55026eea
AndrzejPodo <andrzejpodobinski2@gmail.com>
2021-11-22 17:48:56 +0100

    bump to 0.4.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.AudioMix.Mixfile do
   use Mix.Project
 
-  @version "0.3.1"
+  @version "0.4.0"
   @github_url "https://github.com/membraneframework/membrane_audio_mix_plugin"
 
   def project do
@@ -32,13 +32,13 @@ defmodule Membrane.AudioMix.Mixfile do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.7.0"},
-      {:membrane_caps_audio_raw, "~> 0.4.0"},
+      {:membrane_core, "~> 0.8.0"},
+      {:membrane_caps_audio_raw, "~> 0.5.0"},
       {:bunch, "~> 1.0"},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.5", only: :dev, runtime: false},
-      {:membrane_file_plugin, "~> 0.6.0", only: :test},
+      {:membrane_file_plugin, "~> 0.7.0", only: :test},
       {:membrane_mp3_mad_plugin, "~> 0.7.0", only: :test}
     ]
   end
