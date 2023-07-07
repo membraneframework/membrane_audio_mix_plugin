1ca4ab2739cad82f578ebdd1baff3247b0c0543d
KarolK99 <konkolisz@gmail.com>
2023-07-07 15:32:19 +0200

    Release 0.15.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.AudioMix.Mixfile do
   use Mix.Project
 
-  @version "0.15.1"
+  @version "0.15.2"
   @github_url "https://github.com/membraneframework/membrane_audio_mix_plugin"
 
   def project do
