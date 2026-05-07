21a56499d7b91539bca0373a8fb8993b4c699cfa
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-07 17:10:03 +0200

    Relax constraints of support dependencies

--- mix.exs
+++ mix.exs
@@ -43,9 +43,9 @@ defmodule Membrane.AudioMix.Mixfile do
       {:membrane_raw_audio_format, "~> 0.12.0"},
       {:unifex, "~> 1.0"},
       {:bunch, "~> 1.3"},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 0.0.0", only: :dev, runtime: false},
       {:membrane_file_plugin, "~> 0.16.0", only: :test},
       {:membrane_mp3_mad_plugin, "~> 0.18.0", only: :test},
       {:membrane_realtimer_plugin, "~> 0.9.0", only: :test}
@@ -54,8 +54,7 @@ defmodule Membrane.AudioMix.Mixfile do
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling],
-      plt_add_apps: [:mix, :syntax_tools]
+      flags: [:error_handling]
     ]
 
     if System.get_env("CI") == "true" do
